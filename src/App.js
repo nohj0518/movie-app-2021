94dcae1aeb0ceeb2523d44a0b51ee849e0ef43bf
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-11-15 02:38:17 +0900

    Learn about map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import { func } from "prop-types";
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    //preventDefault(); 역할
+    //버거킹 먹기 쓰고 Submit했을때
+    //refresh 되지 않도록 해줌
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((current) => [toDo, ...current]);
+  };
   return (
     <html lang="ko">
-      <div>
-        {showing ? <Hello /> : null}
-        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      </div>
+      <body>
+        <h1>My To Dos ({toDos.length}) </h1>
+        <form onSubmit={onSubmit}>
+          <input
+            onChange={onChange}
+            value={toDo}
+            type="text"
+            placeholder="Write your to do..."
+          />
+          <button>Add To Do</button>
+        </form>
+        <hr />
+        <ul>
+          {toDos.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </body>
     </html>
   );
 }
