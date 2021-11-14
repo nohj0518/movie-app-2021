c8d28478e421fe32c8c2c003dc660eadb96ab896
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-11-14 23:18:38 +0900

    Learn about useEffect
    Using effect and deps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { useState, useEffect } from "react";
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  const onlyOnceAPI = () => {
+    console.log("Call API...");
+  };
+  useEffect(onlyOnceAPI, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+    if (keyword !== "" && keyword.length > 5)
+      console.log("SEARCH FOR", keyword);
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
   return (
-    <div>
-      <h1>Welcome React :) test </h1>
-    </div>
+    <html lang="ko">
+      <div>
+        <input
+          value={keyword}
+          onChange={onChange}
+          type="text"
+          placeholder="Search here ..."
+        />
+        <h1>{counter}</h1>
+        <button onClick={onClick}> click me </button>
+      </div>
+    </html>
   );
 }
 export default App;
