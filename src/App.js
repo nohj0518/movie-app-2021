1efcf556187e94582e2f144f2b7785833bc59c63
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-11-14 23:37:28 +0900

    Learn about Clean up

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
+import { func } from "prop-types";
 import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  const onlyOnceAPI = () => {
-    console.log("Call API...");
-  };
-  useEffect(onlyOnceAPI, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-    if (keyword !== "" && keyword.length > 5)
-      console.log("SEARCH FOR", keyword);
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <html lang="ko">
       <div>
-        <input
-          value={keyword}
-          onChange={onChange}
-          type="text"
-          placeholder="Search here ..."
-        />
-        <h1>{counter}</h1>
-        <button onClick={onClick}> click me </button>
+        {showing ? <Hello /> : null}
+        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
       </div>
     </html>
   );
