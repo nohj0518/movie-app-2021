1776fc75bfce0facdb9755f0e90db5f4a2177d0f
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-11-16 02:01:18 +0900

    MovieApp

--- src/App.js
+++ src/App.js
@@ -1,48 +1,5 @@
-import { useState, useEffect } from "react";
-
+import Home from "./routes/Home";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  function onChange(event) {
-    setMoney(event.target.value);
-    console.log(money);
-  }
-  return (
-    <html lang="ko">
-      <body>
-        <h1>The Coins💰 {loading ? "" : `(${coins.length})`}</h1>
-        {loading ? (
-          <strong>Loading ...</strong>
-        ) : (
-          <div>
-            <input
-              onChange={onChange}
-              value={money}
-              type="number"
-              placeholder="How much do you have ?"
-            />
-            <select>
-              {coins.map((coin) => (
-                <option>
-                  {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD{" "}
-                  {money / coin.quotes.USD.price}
-                  {coin.symbol}
-                </option>
-              ))}
-            </select>
-          </div>
-        )}
-      </body>
-    </html>
-  );
+  return <Home />;
 }
 export default App;
