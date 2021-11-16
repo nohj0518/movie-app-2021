4e4e57c3688074735c04ffb0c01624c4270972b2
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-11-16 22:15:39 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Routes>
+      <Routes basename={process.env.PUBLIC_URL}>
         <Route path="/movie/:id" element={<Detail />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
